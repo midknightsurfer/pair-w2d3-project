6028b0daf9e2ed404eab808ade2281ddce30ad29
Beau Ferguson <dev@nerdsense.tech>
2021-12-29 15:40:31 -0600

    sixth problem

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,7 +10,13 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// your code here
+const arrowMirrorArray = array => {
+  let result = array;
+    for ( let i = array.length - 1; i >= 0; i--) {
+      result.push(array[i]);
+    }
+    return result;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
